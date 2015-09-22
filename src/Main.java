09296336648b872fc1e6094b1b7ed17ee776287b
heekzz <f.hakansson@hotmail.se>
2015-09-22 20:42:38 +0200

    Initial

---
+++ src/Main.java
@@ -0,0 +1,56 @@
+import java.awt.FlowLayout;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+
+public class Main {
+
+	public static void main (String args[]) {
+		read("log.log");
+		
+		ImageIcon img = new ImageIcon("img/2Q.png");
+		JLabel imgLabel = new JLabel(img);
+		
+		JFrame frame = new JFrame("HFS player");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(new FlowLayout());
+		frame.setVisible(true);
+		frame.add(imgLabel);
+		frame.pack();
+		
+	}
+	
+	public static void read(String fileName) {
+		BufferedReader br = null;
+		
+		try {
+			br = new BufferedReader(new FileReader(fileName));
+			String s;
+			while((s = br.readLine()) != null) {
+				String[] data = s.split(" ");
+				for (int i = 0; i < data.length; i++) {
+					System.out.print(data[i] + "---");
+				}
+				System.out.println();
+			}
+			
+			
+			br.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(br != null) {
+					br.close();
+				}
+			} catch (Exception e2) {
+				e2	.printStackTrace();
+			}
+		}
+	}
+
+}
