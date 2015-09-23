02aa492acd8943c60daf43c85c323925a579e8d2
InspectorDeno <dennisdufback@gmail.com>
2015-09-23 11:47:04 +0200

    Class

--- src/Main.java
+++ src/Main.java
@@ -8,22 +8,9 @@ import javax.swing.JLabel;
 
 
 public class Main {
-	private static final int minBuf = 4;
-	private static final int maxBuf = 6;
-	private static int availableBandwidth;
-	
+
 	public static void main (String args[]) {
 		read("log.log");
-		
-		ImageIcon img = new ImageIcon("img/2Q.png");
-		JLabel imgLabel = new JLabel(img);
-		
-		JFrame frame = new JFrame("HFS player");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setLayout(new FlowLayout());
-		frame.setVisible(true);
-		frame.add(imgLabel);
-		frame.pack();
 		
 	}
 	
@@ -33,18 +20,16 @@ public class Main {
 		try {
 			br = new BufferedReader(new FileReader(fileName));
 			String s;
-			int count = 1;
 			while((s = br.readLine()) != null) {
 				String[] data = s.split(" ");
-//				for (int i = 0; i < data.length; i++) {
-//					System.out.print(data[i] + "---");
-//				}
-				int bit = Integer.parseInt(data[4])*8;
-				int kbit = bit/1000;
-				availableBandwidth = kbit;
-				System.out.println(count++ + ": " + kbit + " Kbit/s");
+				for (int i = 0; i < data.length; i++) {
+					System.out.print(data[i] + "---");
+				}
+				System.out.println();
 			}
 			
+			// Hahah nu är det inte samma igen!
+			// Haha jo det är det lol ava
 			br.close();
 		} catch (Exception e) {
 			e.printStackTrace();
